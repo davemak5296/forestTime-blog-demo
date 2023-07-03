38f98165860b0a2e84683923e44b80a557e0b4a8
Dave Mak <mak9652615@gmail.com>
2023-07-03 14:28:08 +0800

    git init; add design tokens in style.css

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ChildComp from './components/ChildComp.vue'
+
+</script>
+
+<template>
+  <h1 class="">i should inherit styles from body</h1>
+  <ChildComp />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/ChildComp.vue
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<template>
+  <h2>i should be red</h2>
+</template>
+
+<style scoped>
+</style>
