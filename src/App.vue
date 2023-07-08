7d6f59577ffc333b6396606e83f72536e981cec3
Dave Mak <mak9652615@gmail.com>
2023-07-08 10:24:45 +0800

    install fontawesome; add line-height tokens and modify color tokens in style.css

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,15 @@ import ChildComp from './components/ChildComp.vue'
 
 <template>
   <h1 class="">i should inherit styles from body</h1>
+  <fa-icon icon="fa-solid fa-user" class="icon" />
+  <fa-icon :icon="['fas', 'house']" class="icon" />
   <ChildComp />
 </template>
 
 <style scoped>
+.icon {
+  margin-left: 12px;
+  font-size: 36px;
+  color: firebrick;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './style.css'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faUser, faClock, faHouse } from '@fortawesome/free-solid-svg-icons';
 
-createApp(App).mount('#app')
+library.add(faUser, faClock, faHouse);
+
+const app = createApp(App);
+app.component('fa-icon', FontAwesomeIcon);
+app.mount('#app');
